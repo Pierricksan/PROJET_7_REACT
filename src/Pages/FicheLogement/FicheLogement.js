7e7102ca4ef46e29c8d965c42635020ac83ce5ae
Pierrick Rousselot <rousselot.pierrick@gmail.com>
2022-12-21 21:21:06 +0100

    Projet KASA version 5.0.3 [clean] : modification de la page Logement

--- src/Pages/FicheLogement/FicheLogement.js
+++ src/Pages/FicheLogement/FicheLogement.js
@@ -1,37 +1,44 @@
 import React from "react";
+// import modules
 import { Fragment, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+// import components
 import Carroussel from "../../components/Carroussel/Carroussel";
 import Collapse from "../../components/Collapse/Collapse";
 import User from "../../components/User/User";
 import Title from "../../components/Title/Title";
 import Tag from "../../components/Tag/Tag";
 import Rating from "../../components/Rating/Rating";
+// import CSS
 import "./FicheLogement.css";
-
+// import data JSON
 import dataLogement from "../../API/annonce.json";
 
-
+// construction de la fonction d'affichage de la page Logement
 const FicheLogement = () => {
-  /// navigate
+  // constitution du variable de navigation a réutiliser pour la redirection
   const navigate = useNavigate();
 
-  //// UseParams pour l'url
+  // UseParams pour l'url
   let { id } = useParams();
+  // constante et méthode array pour retrouver l'id dans la base de données en fonction du param de l'url
   const idLogement = dataLogement.find(p => p.id === id);
 
+  // appel redirection en cas de mauvaise entrée url ou id non trouvée dans la base de données
   useEffect(() => {
     if (idLogement === undefined) {
       navigate("/erreur");
     }
   });
 
+  // const de style pour personnaliser le collapse
   const fontContent = {
     paddingTop: "20px",
     paddingBottom: "20px",
     textDecoration: "none",
   };
 
+  // condition pour afficher la page si l'ID est bien retrouvé dans la base de données
   if (idLogement !== undefined) {
     return (
       <Fragment>
