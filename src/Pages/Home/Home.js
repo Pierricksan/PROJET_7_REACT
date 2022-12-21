93beb58a41481e4b51512090e49147c26ea64e6a
Pierrick Rousselot <rousselot.pierrick@gmail.com>
2022-12-21 21:15:32 +0100

    Projet KASA version 5.0.2 [clean] : modification nomination CSS page Home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,15 @@
 import React from "react";
+// import module
 import { Fragment } from "react";
 import { Link } from "react-router-dom";
-import styles from "./Home.module.css";
+// import CSS
+import "./Home.css";
+// Import components
 import Banner from "../../components/Banner/Banner";
 import Thumb from "../../components/Thumb/Thumb";
 import image from "../../images/ImagesComponents/IMG.png";
 // import du fichier json
-import data from '../../API/annonce.json';
+import data from "../../API/annonce.json";
 
 const Home = () => {
   // const [data, setData] = useState([]);
@@ -31,17 +34,22 @@ const Home = () => {
   // }, []);
   return (
     <Fragment>
-      <div className={styles.bannerHome}>
-      <Banner imageBanner={image} descriptionAlt="un paysage de montagne avec brouillard et quelques arbres">Chez vous, <br className={styles.disabled}/> partout et ailleurs</Banner>
+      <div className="bannerHome">
+        <Banner
+          imageBanner={image}
+          descriptionAlt="un paysage de montagne avec brouillard et quelques arbres"
+        >
+          Chez vous, <br className="disabled" /> partout et ailleurs
+        </Banner>
       </div>
-      
-      <div className={styles.flexHome}>
-        <div className={styles.containerHomeLocations}>
-          {data.map(({id, title, cover}, index) => {
-            return(
-            <Link key={index} to={`/logement/${id}`}>
-              <Thumb sourceImg={cover}>{title}</Thumb>
-            </Link>);
+      <div className="flexHome">
+        <div className="containerHomeLocations">
+          {data.map(({ id, title, cover }, index) => {
+            return (
+              <Link key={index} to={`/logement/${id}`}>
+                <Thumb sourceImg={cover}>{title}</Thumb>
+              </Link>
+            );
           })}
         </div>
       </div>
